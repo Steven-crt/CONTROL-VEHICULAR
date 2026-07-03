a28ab2c9cd179577e815410989dd79a156a76ef6
Steven-crt <stevensaman968@gmail.com>
2026-07-03 15:06:09 -0500

    Clean up api entry and finalize

--- api/index.js
+++ api/index.js
@@ -1,18 +1,8 @@
-// Explicit requires to prevent Vercel tree-shaking
 require('mysql2');
 require('bcryptjs');
 require('express');
 require('sequelize');
 require('jsonwebtoken');
 
-let handler;
-try {
-  const app = require('../backend/src/app');
-  handler = app;
-} catch (err) {
-  console.error('INIT ERROR:', err.message);
-  handler = (req, res) => {
-    res.status(500).json({ ok: false, error: err.message, stack: err.stack?.split('\n').slice(0, 15) });
-  };
-}
-module.exports = handler;
+const app = require('../backend/src/app');
+module.exports = app;
